aca6eaa6d20f4ae1566c75ceb63c3ccb2ba21218
hwangjeonghyeon1 <deert202@naver.com>
2021-04-26 22:12:03 +0900

    [feat] 개발환경셋팅

---
+++ src/main/java/com/hanshin/shop/config/DbConfig.java
@@ -0,0 +1,30 @@
+package com.hanshin.shop.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class DbConfig {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        Resource[] arrResource = new PathMatchingResourcePatternResolver()
+                .getResources("classpath:mapper/*Mapper.xml");
+        sqlSessionFactoryBean.setMapperLocations(arrResource);
+        sqlSessionFactoryBean.getObject().getConfiguration().setMapUnderscoreToCamelCase(true);
+        return sqlSessionFactoryBean.getObject();
+    }
+
+}
---
+++ src/main/java/com/hanshin/shop/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.hanshin.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
