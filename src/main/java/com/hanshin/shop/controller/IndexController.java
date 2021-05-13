95bdafd0d3f0e832519ac669d380b8207f5fbd5e
HWANGJEONGHYEON1 <deert202@naver.com>
2021-05-14 00:05:13 +0900

    [refactor] indexController에서 권한이 있으면 username 반환

--- src/main/java/com/hanshin/shop/controller/IndexController.java
+++ src/main/java/com/hanshin/shop/controller/IndexController.java
@@ -1,16 +1,22 @@
 package com.hanshin.shop.controller;
 
+import com.hanshin.shop.utill.SecurityUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
+    @GetMapping(value = {"/", "/index"})
+    public String index(Model model) {
+        final String username = SecurityUtil.getCurrentUsername().get();
+        if (!username.equals("anonymousUser")) {
+            model.addAttribute("userName", username);
+        }
+
         return "index";
     }
-
 }
