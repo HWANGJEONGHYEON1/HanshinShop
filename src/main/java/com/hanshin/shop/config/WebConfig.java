2d5d55a17a1360ce74f1369bec8f0b82b05e6d79
HWANGJEONGHYEON1 <deert202@naver.com>
2021-05-20 11:06:49 +0900

    [feat] argumentReslover 등록

--- src/main/java/com/hanshin/shop/config/WebConfig.java
+++ src/main/java/com/hanshin/shop/config/WebConfig.java
@@ -1,9 +1,21 @@
 package com.hanshin.shop.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
 
+    public WebConfig(LoginUserArgumentResolver loginUserArgumentResolver) {
+        this.loginUserArgumentResolver = loginUserArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
 }
