81753e2c82c3c39e1b0d001737a34af43d3c55fe
HWANGJEONGHYEON1 <deert202@naver.com>
2021-05-20 11:23:41 +0900

    [refactor] sysout 제거

--- src/main/java/com/hanshin/shop/config/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/hanshin/shop/config/jwt/JwtAccessDeniedHandler.java
@@ -1,5 +1,6 @@
 package com.hanshin.shop.config.jwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -9,12 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         //필요한 권한이 없이 접근하려 할때 403
-        System.out.println("### 403");
+        log.error("#JwtAccessDeniedHandler 403 ");
         response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 }
