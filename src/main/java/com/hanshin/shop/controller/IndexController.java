9632bb8474ab58ec460fa0c0f75a72ba9ca159ee
HWANGJEONGHYEON1 <deert202@naver.com>
2021-05-20 11:11:01 +0900

    [refactor] User 정보 LoginUser로 변경

--- src/main/java/com/hanshin/shop/controller/IndexController.java
+++ src/main/java/com/hanshin/shop/controller/IndexController.java
@@ -1,20 +1,27 @@
 package com.hanshin.shop.controller;
 
+import com.hanshin.shop.entity.LoginUser;
+import com.hanshin.shop.entity.User;
 import com.hanshin.shop.utill.SecurityUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class IndexController {
 
     @GetMapping(value = {"/", "/index"})
-    public String index(Model model) {
+    public String index(Model model, @LoginUser User user) {
         final String username = SecurityUtil.getCurrentUsername().get();
+
         if (!username.equals("anonymousUser")) {
-            model.addAttribute("userName", username);
+            log.info(user.getName());
+            log.info(user.getEmail());
+            model.addAttribute("userName", user.getName());
         }
 
         return "index";
