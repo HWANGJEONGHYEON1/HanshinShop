7e01fd681c7667d685e4d278a07494bccf76b5e1
HWANGJEONGHYEON1 <deert202@naver.com>
2021-05-24 22:24:17 +0900

    [feat] 에러페이지 및 @ControllerAdvice 추가

--- src/main/java/com/hanshin/shop/exception/CommonExceptionHandler.java
+++ src/main/java/com/hanshin/shop/exception/CommonExceptionHandler.java
@@ -2,18 +2,19 @@ package com.hanshin.shop.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@RestControllerAdvice
+
+@ControllerAdvice
 @Slf4j
-public class ExceptionHandler {
+public class CommonExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler
     public String errorHandler(Exception e, Model model) {
-        log.error("Exception ... " + ex.getMessage());
-        model.addAttribute("exception", ex);
-        log.error(model);
 
-        return "error_page";
+        log.error("Exception ... " + e);
+        model.addAttribute("exception", e);
+        return "/error/error_page";
     }
 }
