eb03ef37c3c0b9e98587010b6103d836d1d27073
HWANGJEONGHYEON1 <deert202@naver.com>
2021-05-31 22:05:36 +0900

    [feat] 파입 업로드 관련 로직

--- src/main/java/com/hanshin/shop/entity/AttachFileDto.java
+++ src/main/java/com/hanshin/shop/entity/AttachFileDto.java
@@ -1,2 +1,31 @@
-package com.hanshin.shop.entity;public class AttachFileDTO {
+package com.hanshin.shop.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class AttachFileDto {
+    private String fileName;
+    private String uploadPath;
+    private String uuid;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getUploadPath() {
+        return uploadPath;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public static AttachFileDto makeBinding(String uploadFileName, String uploadFolderPath, String uuid) {
+        return new AttachFileDto(uploadFileName, uploadFolderPath, uuid);
+    }
 }
