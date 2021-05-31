83aa6c1f89d2164a44cb29e7b8cab07443e53e8f
HWANGJEONGHYEON1 <deert202@naver.com>
2021-05-31 22:07:42 +0900

    [feat] 상품 상세화면 컨트롤러

--- src/main/java/com/hanshin/shop/controller/shop/GoodsDetailController.java
+++ src/main/java/com/hanshin/shop/controller/shop/GoodsDetailController.java
@@ -1,48 +1,53 @@
 package com.hanshin.shop.controller.shop;
 
-import com.hanshin.shop.entity.CategoryVO;
 import com.hanshin.shop.entity.Goods;
 import com.hanshin.shop.entity.GoodsAttachVO;
 import com.hanshin.shop.repository.CategoryMapper;
 import com.hanshin.shop.repository.GoodsAttachMapper;
-import com.hanshin.shop.repository.GoodsMapper;
+import com.hanshin.shop.service.GoodsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/shop-details/")
-public class ShopDetailController {
-    private final GoodsMapper goodsMapper;
+@RequestMapping("/goods-detail")
+public class GoodsDetailController {
+    private final GoodsService goodsService;
     private final GoodsAttachMapper goodsAttachMapper;
     private final CategoryMapper categoryMapper;
 
     @GetMapping("")
-    public String shopDetail() {
-        return "shop/shop-details";
+    public String goodsDetail() {
+        return "goods/goods-details";
     }
 
     @GetMapping("/{id}")
-    public String shopDetail(@PathVariable Long id, Model model) {
-        log.info("#### {}", id);
-        final Goods goods = goodsMapper.findOne(id);
+    public String goodsDetail(@PathVariable Long id, Model model) {
+        final Goods goods = goodsService.findOne(id);
         final List<GoodsAttachVO> attach = goodsAttachMapper.findByGoodsId(id);
         final String categoryName = categoryMapper.get(goods.getCategoryId()).getName();
-        log.info("# attach {}",goods);
-        log.info("# attach {}",attach);
         model.addAttribute("goods", goods);
         model.addAttribute("attach", attach.get(0));
         model.addAttribute("categoryName", categoryName);
 
-        return "shop/shop-details";
+        return "goods/goods-details";
+    }
+
+    @GetMapping("/category/{id}")
+    @ResponseBody
+    public ResponseEntity<List<Goods>> categories(@PathVariable Long id) {
+        return new ResponseEntity<>(goodsService.findListOfRelated(id), HttpStatus.OK);
     }
 
 }
