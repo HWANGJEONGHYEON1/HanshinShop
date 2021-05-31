f5f17d8592c129d8e4923d5b66c2b1387d4ed8ee
HWANGJEONGHYEON1 <deert202@naver.com>
2021-05-31 22:09:34 +0900

    [feat] 테스트추가

--- src/test/java/com/hanshin/shop/repository/CategoryMapperTest.java
+++ src/test/java/com/hanshin/shop/repository/CategoryMapperTest.java
@@ -1,4 +1,38 @@
+package com.hanshin.shop.repository;
+
+import com.hanshin.shop.entity.CategoryVO;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class CategoryMapperTest {
-  
+
+    @Autowired
+    private CategoryMapper mapper;
+
+
+    @Test
+    public void 카테고리_정보() throws Exception {
+        //given
+        final CategoryVO categoryVO = mapper.get(2L);
+        //when then
+        Assertions.assertThat(categoryVO.getName()).isEqualTo("야채");
+
+    }
+
+    @Test
+    public void 카테고리_정보_목록() throws Exception {
+
+        //when
+
+        final List<CategoryVO> select = mapper.list();
+        //then
+        Assertions.assertThat(select.size()).isEqualTo(5);
+    }
 }
--- src/test/java/com/hanshin/shop/service/GoodsServiceTest.java
+++ src/test/java/com/hanshin/shop/service/GoodsServiceTest.java
@@ -1,4 +1,42 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.hanshin.shop.service;
+
+import com.hanshin.shop.entity.Goods;
+import com.hanshin.shop.entity.GoodsDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
 class GoodsServiceTest {
-  
+
+    private static Logger log = LoggerFactory.getLogger(GoodsServiceTest.class);
+
+    @Autowired
+    private GoodsService goodsService;
+
+
+    @Test
+    public void 화면에_뿌려줄_상품() throws Exception {
+        //given
+        final List<Goods> allIncludeAttach = goodsService.findAllList();
+        Assertions.assertThat(allIncludeAttach.size()).isEqualTo(goodsService.findAllList().size());
+        //then
+    }
+    @Test
+    public void 세이브() throws Exception {
+        //given
+        GoodsDto dto = new GoodsDto("test", 40000, "Test 설명");
+        Goods goods = Goods.create(dto);
+        //when
+        goodsService.save(goods);
+        System.out.println("#"+ goods.getId());
+        //then
+        Assertions.assertThat(goodsService.findOne(goods.getId()).getName()).isEqualTo("test");
+    }
+
 }
