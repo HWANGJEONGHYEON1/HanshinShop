22b0a2ff8063cf1058fdfcdc3983623727587459
HWANGJEONGHYEON1 <deert202@naver.com>
2021-06-04 20:22:07 +0900

    [refactor] cart mvc 추가

--- src/main/java/com/hanshin/shop/entity/cart/CartDTO.java
+++ src/main/java/com/hanshin/shop/entity/cart/CartDTO.java
@@ -1,2 +1,13 @@
-package com.hanshin.shop.entity.cart;public class CartDTO {
+package com.hanshin.shop.entity.cart;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CartDTO {
+
+    private Long userId;
+    private Long goodsId;
+    private int amount;
 }
--- src/main/java/com/hanshin/shop/service/CartService.java
+++ src/main/java/com/hanshin/shop/service/CartService.java
@@ -1,2 +1,50 @@
-package com.hanshin.shop.service;public class CartService {
+package com.hanshin.shop.service;
+
+import com.hanshin.shop.entity.cart.CartVO;
+import com.hanshin.shop.repository.CartMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CartService {
+
+    private final CartMapper cartMapper;
+
+    @Transactional
+    public void insert(CartVO cartVO) {
+        final CartVO existCartVO = isExistCartOne(cartVO.getGoodsId(), cartVO.getUserId());
+        log.info("existVO {}", existCartVO);
+
+        if (Objects.isNull(existCartVO)) {
+            cartMapper.insert(cartVO);
+        } else {
+            log.info("existVO {}", existCartVO);
+            cartMapper.update(cartVO.getAmount(), existCartVO.getId());
+        }
+    }
+
+    public List<CartVO> findAll(Long userId) {
+        return cartMapper.findAll(userId);
+    }
+
+    public CartVO isExistCartOne(Long goodsId, Long userId) {
+        return cartMapper.isExistCartOne(goodsId, userId);
+    }
+
+    @Transactional
+    public int delete(Long id) {
+        return cartMapper.delete(id);
+    }
+
+    @Transactional public int deleteAll(Long userId) {
+        return cartMapper.deleteAll(userId);
+    }
 }
