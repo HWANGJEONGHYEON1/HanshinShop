919157c2e8c25de8641bcbd95cfa80ae8742681e
HWANGJEONGHYEON1 <deert202@naver.com>
2021-06-16 15:36:58 +0900

    [feat] paging처리 객체 추가

---
+++ src/test/java/com/hanshin/shop/controller/shop/GoodsControllerTest.java
@@ -0,0 +1,43 @@
+package com.hanshin.shop.controller.shop;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+class GoodsControllerTest {
+
+    @Autowired
+    private WebApplicationContext ctx;
+
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+
+
+    @Test
+    public void paging() throws Exception {
+        final MvcResult mvcResult = mockMvc.perform(
+                get("/goods/main")
+                        .param("pagaNum", "2")
+                        .param("amount", "50")
+        ).andReturn();
+        System.out.println(mvcResult.getRequest());
+        System.out.println(mvcResult.getResponse());
+
+    }
+}
