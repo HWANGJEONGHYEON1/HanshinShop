88e47070a7f13822256b9100ceb8ffcffa6acfd9
HWANGJEONGHYEON1 <deert202@naver.com>
2021-06-17 11:44:15 +0900

    [feat] api exception 처리 -> @RestControllerAdvice

--- src/main/java/com/hanshin/shop/exception/ApiExceptionHandler.java
+++ src/main/java/com/hanshin/shop/exception/ApiExceptionHandler.java
@@ -1,2 +1,15 @@
-package com.hanshin.shop.exception;public class ApiExceptionHandler {
+package com.hanshin.shop.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ApiExceptionHandler {
+
+    @ExceptionHandler
+    public String errorHandler(Exception e) {
+        return e.getMessage();
+    }
 }
