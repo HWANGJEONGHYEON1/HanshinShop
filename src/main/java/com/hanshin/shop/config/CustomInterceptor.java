d29b3695c5598951bf6e8ef3a80a8ba6ff534a28
HWANGJEONGHYEON1 <deert202@naver.com>
2021-06-17 11:43:35 +0900

    [feat] 응답 전 후 로그  인터셉터 추가 -> 로그

--- src/main/java/com/hanshin/shop/config/CustomInterceptor.java
+++ src/main/java/com/hanshin/shop/config/CustomInterceptor.java
@@ -1,2 +1,31 @@
-package com.hanshin.shop.config;public class CustomInterceptor {
+package com.hanshin.shop.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class CustomInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("# request.getRequestURI() : {}", request.getRequestURI());
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("# response status : {}", response.getStatus());
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
 }
