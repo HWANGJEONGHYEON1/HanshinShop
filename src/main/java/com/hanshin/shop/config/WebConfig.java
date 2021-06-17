83a730c8d722a0784a44b45397725c83ff60ab07
HWANGJEONGHYEON1 <deert202@naver.com>
2021-06-17 11:43:14 +0900

    [feat] 응답 전 후 로그  인터셉터 추가 -> 로

--- src/main/java/com/hanshin/shop/config/WebConfig.java
+++ src/main/java/com/hanshin/shop/config/WebConfig.java
@@ -1,24 +1,29 @@
 package com.hanshin.shop.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final LoginUserArgumentResolver loginUserArgumentResolver;
-
-    public WebConfig(LoginUserArgumentResolver loginUserArgumentResolver) {
-        this.loginUserArgumentResolver = loginUserArgumentResolver;
-    }
+    private final CustomInterceptor interceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor).addPathPatterns("/**");
     }
 
     @Bean
