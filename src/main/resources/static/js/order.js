e94e3783ea6887214d94c93c9495313f286d26cc
HWANGJEONGHYEON1 <deert202@naver.com>
2021-06-19 01:04:09 +0900

    [refactor] 주문 내역 화면 수정 아이디 추가 및 전체 주문취소 삭제/ 개인주문 취소 추가

--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -18,25 +18,29 @@
             dataType: 'json',
             type: 'get',
             success: function (result) {
-                console.log(result)
+
                 let orderArray = []
                 let orderTotal = 0;
                 let date;
                 data = result;
+
                 for (let orders in result) {
+
                     let order = result[orders];
                     let path = getPath(order);
-                    let total = order.orderPrice * order.amount;
-                    orderTotal += total;
+                    let total = 0;
                     date = convertDate(order.orderDate);
                     orderArray.push(`
                         <tr>
+                            <td class="shoping__cart__price">
+                                ${order.id}
+                            </td>
                             <td class="shoping__cart__item">
                                 <img src="${path}" alt="" style="width: 200px; height: 200px;">
                                 <h5>${order.name}</h5>
                             </td>
                             <td class="shoping__cart__price">
-                                ${total}
+                                ${order.orderPrice}
                             </td>
                             <td class="shoping__cart__price">
                                 ${order.amount}
@@ -50,24 +54,59 @@
                             <td class="shoping__cart__total">
                                 ${order.state}
                             </td>
+                            <td class="shoping__cart__total">
+                               <button class="ui-icon-cancel cancel" 
+                                    data-orderid="${order.id}" 
+                                    data-state="${order.state}"
+                                    >
+                                    주문취소
+                               </button>
+                            </td>
                         </tr>
                     `);
+                    console.log(order.state);
+                    if (order.state == "ORDER") {
+                        total = order.orderPrice * order.amount;
+                    }
+                    orderTotal += total;
                 }
                 $("#orders").empty();
                 $("#orders").append(orderArray);
+
                 $("#totalPrice").text(orderTotal);
             }
         });
     }
 
-    $("#orderCancelBtn").on("click", function(e) {
+    $(document).on("click", ".cancel", function (e) {
+        console.log($(this).data("orderid"));
+        let orderId = $(this).data("orderid");
+        let state = $(this).data("state");
+
+        if (state == 'CANCEL') {
+            alert("이미 주문 취소 되었습니다.");
+            return;
+        }
+
+        $.ajax({
+            url: '/api/order/cancel/' + orderId,
+            method: "PATCH",
+            dataType: 'text',
+            success: function (result) {
+                alert('주문 번호 '+ orderId+ ' 취소되었습니다.');
+                orderList();
+            }
+        })
+    })
+
+
+    $("#orderAllCancelBtn").on("click", function(e) {
         console.log("## orederCancel btn click");
         $.ajax({
             url: '/api/order/cancel',
-            method: "patch",
+            method: "PATCH",
             dataType: 'text',
             success: function (result) {
-                console.log(result);
                 alert("주문취소 되었습니다.");
                 orderList();
             }
