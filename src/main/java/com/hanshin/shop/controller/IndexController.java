4159dc58c92f5cd32a8f8af4b429ef1ec07e3611
HWANGJEONGHYEON1 <deert202@naver.com>
2021-06-29 14:08:17 +0900

    [refactor] admin 코드 model에 담아 처리

--- src/main/java/com/hanshin/shop/controller/IndexController.java
+++ src/main/java/com/hanshin/shop/controller/IndexController.java
@@ -1,12 +1,16 @@
 package com.hanshin.shop.controller;
 
 import com.hanshin.shop.vo.user.LoginUser;
+import com.hanshin.shop.vo.user.RoleType;
 import com.hanshin.shop.vo.user.User;
+import com.hanshin.shop.vo.user.UserRole;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Objects;
 
 @Controller
 @Slf4j
@@ -14,6 +18,10 @@ public class IndexController {
 
     @GetMapping(value = {"/", "/index"})
     public String index(Model model, @LoginUser User user) {
+        if (!Objects.isNull(user)) {
+            boolean isAdmin = user.getUserRoles().get(0).getRole() == RoleType.ROLE_ADMIN ? true : false;
+            model.addAttribute("isAdmin", isAdmin);
+        }
         return "index";
     }
 
