ec5f8a7ec09c245c06a116a32406d37f1606f630
hwangjeonghyeon1 <jhhwang@wemakeprice.com>
2021-12-03 18:31:44 +0900

    [feat] test-redis 기능 추가

--- src/test/java/com/hanshin/shop/service/GoodsServiceTest.java
+++ src/test/java/com/hanshin/shop/service/GoodsServiceTest.java
@@ -1,22 +1,24 @@
 package com.hanshin.shop.service;
 
 
+import com.hanshin.shop.redis.TestRedisConfiguration;
 import com.hanshin.shop.vo.goods.Goods;
-import com.hanshin.shop.vo.goods.GoodsAttachVO;
 import com.hanshin.shop.vo.paging.Criteria;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
 import java.util.List;
 
-@SpringBootTest
-@Transactional
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TestRedisConfiguration.class)
 class GoodsServiceTest {
 
     private static Logger log = LoggerFactory.getLogger(GoodsServiceTest.class);
@@ -25,21 +27,10 @@ class GoodsServiceTest {
     private GoodsService goodsService;
 
     @Test
-    public void 화면에_뿌려줄_상품() throws Exception {
-        //given
-        Criteria criteria = new Criteria(1,10);
-        final List<Goods> allIncludeAttach = goodsService.findAllList(criteria);
-        Assertions.assertThat(allIncludeAttach.size()).isEqualTo(goodsService.findAllList(criteria).size());
-        //then
-    }
-
-    @Test
-    public void 저장() throws Exception {
-        //given
-        List<GoodsAttachVO> list = Arrays.asList(new GoodsAttachVO("22dacfc8-b9c0-4771-bc37-0bfd86874f24", "2021/05/31", "product-4.jpg"));
-//        Goods goods = c
-        //when
-
-        //then
+    @DisplayName("전체 상품 리스트 수는 2이다.")
+    public void all_list_size_2() {
+        Criteria criteria = new Criteria(5, 10);
+        List<Goods> allList = goodsService.findAllList(criteria);
+        Assertions.assertThat(2).isEqualTo(allList.size());
     }
 }
