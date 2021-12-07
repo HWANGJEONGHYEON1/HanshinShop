6743c569441b9e55865a224ab2e261fccebab587
hwangjeonghyeon1 <jhhwang@wemakeprice.com>
2021-12-07 12:43:35 +0900

    [refactor] 첨부파일 없을 시 에러 클래스 추가

---
+++ src/main/java/com/hanshin/shop/config/exception/AttachmentNotExistException.java
@@ -0,0 +1,9 @@
+package com.hanshin.shop.config.exception;
+
+public class AttachmentNotExistException extends RuntimeException {
+    private final static String message = "첨부파일이 존재하지 않습니다.";
+
+    public AttachmentNotExistException() {
+        super(message);
+    }
+}
