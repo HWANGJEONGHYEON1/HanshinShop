b17d0c9eca058f30199834bbed3d06656f36cdb3
hwangjeonghyeon1 <jhhwang@wemakeprice.com>
2021-12-10 18:56:13 +0900

    [refactor] 클래스명 변경

--- src/test/java/com/hanshin/shop/repository/UserMapperTest.java
+++ src/test/java/com/hanshin/shop/repository/UserMapperTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class UserMapperTests extends IntegrationTests {
+public class UserMapperTest extends IntegrationTests {
 
     @Autowired
     private UserMapper mapper;
